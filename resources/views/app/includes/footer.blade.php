cff8bdfa3364d437632b04ed1a066a54e0f27e19
Ben Jones <ben@bnjns.uk>
2018-07-04 22:32:57 +0100

    Improve footer layout
    Improve the ordering of the footer elements, and I totally definitely
    did not forget to make it mobile friendly before ...

--- resources/views/app/includes/footer.blade.php
+++ resources/views/app/includes/footer.blade.php
@@ -1,11 +1,11 @@
 <div id="footer--upper">
     <div class="container">
-        <div id="footer--upper--about">
-            <h1>Contact Us</h1>
+        <div id="footer--about">
             <div>
                 <span class="fa fa-envelope"></span>
                 <a href="mailto:info@bts-crew.com">info@bts-crew.com</a>
             </div>
+
             <div>
                 <span class="fa fa-phone"></span>
                 +44 (0) 1225 383067
@@ -19,35 +19,33 @@
                 The SU, University of Bath, Bath, BA2 7AY
             </div>
         </div>
-        <div id="footer--upper--links">
-            <ul>
-                <li><a href="{{ route('contact.enquiries') }}">Contact Us</a></li>
-                <li><a href="{{ route('contact.book') }}">Book Us</a></li>
-                <li><a href="{{ route('contact.book.terms') }}">Terms and Conditions</a></li>
-                <li><a href="{{ route('page.show', ['slug' => 'faq']) }}">Frequently Asked Questions</a></li>
-                <li><a href="{{ route('page.show', ['slug' => 'links']) }}">Useful Links</a></li>
-                <li><a href="{{ route('page.show', ['slug' => 'privacy-policy']) }}">Privacy Policy</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
-<div id="footer--lower">
-    <div class="container">
-        <div id="footer--lower--social">
+        <div id="footer--social">
             <a href="https://www.facebook.com/BackstageTechnicalServices" target="_blank">
                 <span class="fa fa-facebook" title="Follow us on facebook"></span>
             </a>
             <a href="https://www.twitter.com/backstagetech" target="_blank">
                 <span class="fa fa-twitter" title="Follow us on twitter"></span>
             </a>
-            <a href="https://plus.google.com/104029009513703982895" target="_blank">
-                <span class="fa fa-google-plus" title="Follow us on G+"></span>
-            </a>
             <a href="https://www.github.com/backstagetechnicalservices/website/issues" target="_blank">
                 <span class="fa fa-github" title="Report an issue"></span>
             </a>
         </div>
-        <div id="footer--lower--copyright">
+    </div>
+</div>
+<div id="footer--lower">
+    <div class="container">
+        <div id="footer--links">
+            <ul>
+                <li><a href="{{ route('contact.enquiries') }}">Contact Us</a></li>
+                <li><a href="{{ route('contact.book') }}">Book Us</a></li>
+                <li><a href="{{ route('page.show', ['slug' => 'links']) }}">Useful Links</a></li>
+                <li><a href="{{ route('page.show', ['slug' => 'faq']) }}">FAQ</a></li>
+                <li><a href="{{ route('contact.book.terms') }}">Terms and Conditions</a></li>
+                <li><a href="{{ route('page.show', ['slug' => 'privacy-policy']) }}">Privacy Policy</a></li>
+            </ul>
+
+        </div>
+        <div id="footer--copyright">
             <p>&copy; Backstage Technical Services {{ date('Y') }}. All rights reserved.</p>
             <p>Generously supported by the University of Bath Alumni Fund.</p>
         </div>
