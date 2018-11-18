8cd4f2ca96eb5e71327730c49c2fde5bd8e59945
Jordan Kirk <jordanlkirk99@gmail.com>
2018-11-18 19:00:06 +0000

    Improves skill welcome message

--- resources/views/training/skills/panes/welcome.blade.php
+++ resources/views/training/skills/panes/welcome.blade.php
@@ -1,8 +1,6 @@
 <h3>Welcome to the training skills section</h3>
-<p>It is required by the Students' Union that we keep accurate records of our membership's abilities so they will be confident that we know
-    what we're doing and do it safely and to the relevant standards. We can also use these skills to help us plan any training to ensure
-    that we retain the skills necessary to do what we do.</p>
-<p>This section provides details on what skills are available, who has what and what you need to do be eligble for a skill. If you feel you
-    should have a skill then you can use the 'Propose a Skill' function - this alerts the Training and Safety Officer, who will review your
-    proposal.</p>
-<p>If you are 'Level 3' in a skill then you are also allowed to award that skill to other members, using the 'Award skill' function.</p>
+<p>The SU requires us to keep accurate records of the membership's abilities, to ensure we're safe and following the relavent standards.
+     These records are also used to plan training to ensure skills are retained within the membership.</p>
+<p>Here you may browse the avaliable skills and what they require. If you feel eligable for any skill click the 'Apply for Skill' below,
+    the Training and Safety Officer will review your application.</p>
+<p>Members who have a 'Level 3' in a skill may also award that skill to other members, using the 'Award Skill' button below.</p>
