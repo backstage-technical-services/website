bf0178f79e57fc71b619ed376f7bf28c76b59496
Jordan Kirk <jordanlkirk99@gmail.com>
2019-01-19 17:17:04 +0000

    Transfer JSON paperwork data to new paperwork tables

--- database/migrations/2019_01_19_162406_create_paperwork_tables.php
+++ database/migrations/2019_01_19_162406_create_paperwork_tables.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreatePaperworkTables extends Migration
 {
@@ -13,11 +14,12 @@ class CreatePaperworkTables extends Migration
      */
     public function up()
     {
+        // --- Table Creation ---
         // Possible Paperwork
         Schema::create('event_paperwork', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('title');
+            $table->string('title')->nullable();
             $table->text('description')->nullable();
             $table->text('template_link')->nullable();
             $table->timestamps();
@@ -43,6 +45,36 @@ class CreatePaperworkTables extends Migration
                   ->onUpdate('cascade')
                  ->onDelete('cascade');
         });
+
+        // --- Data Migration ---
+        // Non destructively convert JSON paperwork info from events DB table into new pivot table
+
+        // Local arrays used to reduce DB read and writes
+        $knownPaperwork[] = "";        // Empty entry to match database index
+        $temp_event_event_paperwork = [];
+
+        foreach(DB::table('events') -> get() as $event)
+        {
+            foreach(json_decode($event->paperwork) as $type => $completed)
+            {
+                // Add unseen paperwork to table
+                if (!in_array($type, $knownPaperwork))
+                {
+                    $knownPaperwork[] = $type;
+                    DB::table('event_paperwork') -> insert([ 'name'  => $type]);
+                }
+                // Add paperwork to local array
+                $temp_event_event_paperwork[] = ['event_id'      => $event->id,
+                                                 'paperwork_id'  => array_search($type, $knownPaperwork),
+                                                 'completed'     => $completed ];
+            }
+            // Batch processing to reduce DB writes
+            if(sizeof($temp_event_event_paperwork)>2000)
+            {
+                DB::table('event_event_paperwork') -> insert($temp_event_event_paperwork);
+                $temp_event_event_paperwork = [];
+            }
+        }
     }
 
     /**
