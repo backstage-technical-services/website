616095cfa0cb3c84bf7da7e2bc7e755beebce6ac
Ben Jones <ben@bnjns.uk>
2019-10-17 20:46:09 +0100

    add google analytics

--- resources/views/app/includes/head.blade.php
+++ resources/views/app/includes/head.blade.php
@@ -1,3 +1,12 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-150311689-1"></script>
+<script>window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', 'UA-150311689-1');</script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
