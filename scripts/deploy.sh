df72d0324782188ef5b086e22b895c0a11a8f62b
Ben Jones <ben@bnjns.uk>
2021-02-14 18:57:26 +0000

    new deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+exitInvalidEnv=1
+
+function _error() {
+  echo "${1}" >&2
+}
+
+readonly repository="${1}"
+readonly commit="${2}"
+readonly siteRoot="${3}"
+readonly siteRootPath="/var/www/${siteRoot}"
+
+if [[ -z "${repository}" ]]; then
+  _error "Cannot check repository out: no repository provided"
+  exit $exitInvalidEnv
+fi
+
+if [[ -z "${commit}" ]]; then
+  _error "Cannot check repository out: no commit hash provided"
+  exit $exitInvalidEnv
+fi
+
+if [[ -z "${siteRoot}" ]]; then
+  _error "Cannot check repository out: no site root provided"
+  exit $exitInvalidEnv
+fi
+
+# Check out repository
+echo "=> Cloning to ${siteRootPath}/${commit} ..."
+ssh-keyscan -H github.com >>~/.ssh/known_hosts
+rm -rf "${siteRootPath:?}/${commit}"
+git clone "${repository}" "${siteRootPath}/${commit}"
+pushd "${siteRootPath}/${commit}"
+
+echo "=> Checking out ${commit} ..."
+git checkout "${commit}"
+
+# Dependencies
+echo "=> Installing dependencies ..."
+composer install --no-interaction --no-dev --prefer-dist
+yarn install --no-dev --frozen-lockfile
+echo "=> Building dependencies ..."
+yarn prod
+
+# Copy config
+echo "=> Configuring application ..."
+cp ../active/.env ./.env
+
+# Migrations
+echo "=> Running migrations ..."
+php artisan migrate --force --no-interaction
+
+# Cache
+echo "=> Configuring cache ..."
+php artisan config:cache
+
+# Symlink resources
+ln -s "${siteRootPath}/storage/elections" "${siteRootPath}/${commit}/resources/elections"
+ln -s "${siteRootPath}/storage/resources" "${siteRootPath}/${commit}/resources/resources"
+
+# Configuring permissions
+echo "=> Configuring permissions ..."
+chown -R forge:www-data "${siteRootPath}/${commit}"
+
+# Switch over
+echo "=> Switching over ..."
+current="$(readlink -f "${siteRootPath}/active")"
+rm "${siteRootPath}/active" && ln -s "${siteRootPath}/${commit}" "${siteRootPath}/active"
+chown -R forge:www-data "${siteRootPath}/active"
+rm -rf "${current}"
+
+echo "=> Done."
