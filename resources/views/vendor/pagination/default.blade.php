6e9b4c3319dc8b40135b0fb5ff6137a954f691a4
Ben Jones <ben@bnjns.uk>
2023-04-07 18:32:02 +0100

    include prev/next link classes in paginator
    fixes backstage-technical-services/hub#138

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -2,9 +2,9 @@
     <ul class="pagination">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <li class="disabled"><span>&laquo;</span></li>
+            <li class="prev-link disabled"><span>&laquo;</span></li>
         @else
-            <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
+            <li class="prev-link"><a href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
         @endif
 
         {{-- Pagination Elements --}}
@@ -28,9 +28,9 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
+            <li class="next-link"><a href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
         @else
-            <li class="disabled"><span>&raquo;</span></li>
+            <li class="next-link disabled"><span>&raquo;</span></li>
         @endif
     </ul>
 @endif
