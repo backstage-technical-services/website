d2d6571b116d9e537e5b16d09eff9139b7e9e746
Ben Jones <ben@bnjns.uk>
2024-07-04 11:36:04 +0100

    fix: fix backup config location

--- config/backup.php
+++ config/backup.php
@@ -19,7 +19,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name'                         => env('APP_NAME', 'backups'),
+        'name'                         => 'backups'             ,
         'source'                       => [
             'files'     => [
 
@@ -181,7 +181,7 @@ return [
      */
     'monitor_backups' => [
         [
-            'name'          => env('APP_NAME', 'backups'),
+            'name'          => 'backups',
             'disks'         => ['local'],
             'health_checks' => [
                 MaximumAgeInDays::class          => 1,
