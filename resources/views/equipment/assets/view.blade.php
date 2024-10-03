375dffcdf9ff96c11dabc8a3ab0e83dc3f19a362
J Lucas <30297264+Laserbeam31@users.noreply.github.com>
2024-10-03 18:10:15 +0100

    Update view.blade.php to point to 2024/25 Asset Register

--- resources/views/equipment/assets/view.blade.php
+++ resources/views/equipment/assets/view.blade.php
@@ -10,5 +10,5 @@
     <iframe width="100%"
             height="700"
             frameborder="0"
-            src="https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vRBIJLyKO--ffboCd90BXWBD95DZm99cXXqi9eSnzPkoCMeMIilnRC81udAtD3ghbF4zt5bzeqDLGQb/pubhtml?widget=true&amp;headers=false"></iframe>
+            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQnhQT5kYi24oPgXIfBIoWEekj8TBwtBzM51kMxzxjVMS0OeBQrq9anGDaq0ifdVFB2cmSpnstQH1mO/pubhtml?widget=true&amp;headers=false"></iframe>
 @endsection
