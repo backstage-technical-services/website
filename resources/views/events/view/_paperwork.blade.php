e67064e8024e224ba63cb086148c227ba9345031
Ben Jones <ben@bnjns.uk>
2024-12-10 18:16:28 +0000

    fix: only allow users to update paperwork if they have permission (#44)

--- resources/views/events/view/_paperwork.blade.php
+++ resources/views/events/view/_paperwork.blade.php
@@ -1,6 +1,12 @@
-<span class="editable"
-      data-editable="toggle"
-      data-action="{{ route('event.update', ['id' => $event->id]) }}"
-      data-field="paperwork.{{ $paperwork }}">
-            <span class="fa fa-{{ $event->paperwork[$paperwork] ? 'check' : 'remove' }}"></span>
-        </span>
+@can('update', $event)
+    <span class="editable"
+          data-editable="toggle"
+          data-action="{{ route('event.update', ['id' => $event->id]) }}"
+          data-field="paperwork.{{ $paperwork }}">
+        <span class="fa fa-{{ $event->paperwork[$paperwork] ? 'check' : 'remove' }}"></span>
+    </span>
+@else
+    <span class="editable data-editable data-editable--toggle">
+        <span class="fa fa-{{ $event->paperwork[$paperwork] ? 'check' : 'remove' }}"></span>
+    </span>
+@endcan
