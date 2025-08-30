ec4a7f9d0ae24ec91a9c8a8922359adca98b1817
Ben Jones <ben@bnjns.uk>
2025-08-30 23:47:41 +0100

    test: add tests for static pages

--- tests/Feature/HomepageTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use function Pest\Laravel\get;
-
-it('should show the homepage', function () {
-    get('/')
-       ->assertSuccessful()
-       ->assertSeeText('Backstage Technical Services is a society formed of students at the University of Bath');
-});
