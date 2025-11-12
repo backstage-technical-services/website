f8a18c4356eae5ad6c350478d5254a1172769ff9
Ben Jones <ben@bnjns.uk>
2025-11-12 23:55:42 +0000

    build: add phpstan (#73)

--- tests/Pest.php
+++ tests/Pest.php
@@ -24,10 +24,6 @@ pest()->extend(Tests\TestCase::class)->use(Illuminate\Foundation\Testing\Refresh
 |
 */
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
-
 /*
 |--------------------------------------------------------------------------
 | Functions
@@ -38,8 +34,3 @@ expect()->extend('toBeOne', function () {
 | global functions to help you to reduce the number of lines of code in your test files.
 |
 */
-
-function something()
-{
-    // ..
-}
